4416868f42d4eba74b9df59b4bb937b3f365334e
germalvarez@gmail.com <>
2014-11-16 21:56:13 -0200

    Metodos de escritura y lectura

--- Obligatorio/src/Obligatorio/Sistema.java
+++ Obligatorio/src/Obligatorio/Sistema.java
@@ -3,8 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Obligatorio;
+
+import java.io.IOException;
+import static java.lang.System.console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,8 +21,12 @@ public class Sistema {
     /**
      * @param args the command line arguments
      */
+    ArrayList<Afiliado> afiliados;
+    ArrayList<Medico> medicos;
+    ArrayList<Sucursal> sucursales;
+
     public static void main(String[] args) {
-        // TODO code application logic here
+            // TODO code application logic here
+
     }
-    
 }
